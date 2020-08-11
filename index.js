5ef77994e92bb9a589d24e8f4e42b785d0d2852b
tuvvph09410 <56832432+tuvvph09410@users.noreply.github.com>
2020-08-11 15:55:04 +0700

    tuvvph09401

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var multer = require('multer');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoDB = 'mongodb+srv://admin:admin@cluster0.zxhfs.mongodb.net/test';
-
+var override=require('method-override');
+app.use(override('_method'));
 var db = require('mongoose');
 var Schema = db.Schema;
 var ObjectId = Schema.ObjectId;
@@ -86,10 +87,22 @@ app.get('/login', function (req, res) {
 });
 
 
-app.get('/them', (req, res) => {
+app.get('', (req, res) => {
     res.render('index');
 
 
+});
+app.get('/list', (req, res) => {
+    var danhsach = db.model(SanPham, SP);
+    var kq = danhsach.find({}, function (error, data) {
+        res.render('listLogin', {dulieu: data,
+            helpers: {
+                index_item: function(value){
+                    return parseInt(value) + 1;
+                }
+            }
+        })
+    }).lean();
 });
 app.get('/sua', (req, res) => {
     var id_sv = '5f27608d3bdeed2cf088b237';
@@ -103,6 +116,7 @@ app.get('/sua', (req, res) => {
 });
 app.get('/xoa', (req, res) => {
     var xoa = db.model(SanPham, SP);
+    // var id_sp=req.query.id;
     xoa.find({}).then(function (records) {
 
         records.forEach(function (record) {
@@ -110,7 +124,11 @@ app.get('/xoa', (req, res) => {
             console.log(record._doc._id);
             var kq = xoa.deleteOne({_id: id_sp},
                 function (error) {
-                    res.send('xoa thanh cong, kiem tra lại DB')
+                if (error == null) {
+                    res.send('xoa thanh cong, kiem tra lại DB');
+                }else {
+                    res.send('Không tìm thấy ID ');
+                }
                 }
             )
         });
@@ -119,10 +137,17 @@ app.get('/xoa', (req, res) => {
 
 
 });
-app.get('/danhsach', (req, res) => {
-    var danhsach = db.model(SanPham, SP);
-    var kq = danhsach.find({}, function (error, data) {
-        res.render('listLogin', {dulieu: data})
-    }).lean();
+app.get('/delete', function(req, res){
+    res.render('listLogin');
 });
+app.delete('/list/:id?', function(req, res){
+    var xoa = db.model(SanPham, SP);
+    var kq = xoa.deleteOne({_id: req.params.id}, function(err){
+        res.redirect('/list');
+    });
+});
+app.get('/update',function (req,res) {
+ res.render('update');
+});
+
 app.listen(process.env.PORT || ' 2000');
